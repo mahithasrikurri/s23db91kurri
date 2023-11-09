57c43288d48b5df31c0341fb292b25e259253327
Kurri <S566991@nwmissouri.edu>
2023-11-08 23:16:49 -0600

    added controller/Apple.js

--- controllers/Apple.js
+++ controllers/Apple.js
@@ -40,3 +40,25 @@ res.status(500);
 res.send(`{"error": ${err}}`);
 }
 };
+
+// Handle Apple create on POST.
+exports.Apple_create_post = async function(req, res) {
+console.log(req.body)
+let document = new Apple();
+// We are looking for a body, since POST does not have query parameters.
+// Even though bodies can be in many different formats, we will be picky
+// and require that it be a json object
+// {"costume_type":"goat", "cost":12, "size":"large"}
+document.name = req.body.name;
+document.color = req.body.color;
+document.price = req.body.price;
+try{
+let result = await document.save();
+res.send(result);
+}
+catch(err){
+res.status(500);
+res.send(`{"error": ${err}}`);
+}
+};
+
