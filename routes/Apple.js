9839d23f3b924e71849e08841f43ff69cf0ed75c
Kurri <S566991@nwmissouri.edu>
2023-11-29 17:06:14 -0600

    Apple.pug

--- routes/Apple.js
+++ routes/Apple.js
@@ -1,18 +1,27 @@
 var express = require('express');
 const Apple_controlers= require('../controllers/Apple');
 var router = express.Router();
+
+const secured = (req, res, next) => {
+    if (req.user) {
+        return next();
+    }
+    res.redirect("/login");
+}
+
 /* GET Apple */
 router.get('/', Apple_controlers.Apple_view_all_Page );
-module.exports = router;
 
 /* GET detail Apple page */
 router.get('/detail', Apple_controlers.Apple_view_one_Page);
 
 /* GET create Apple page */
-router.get('/create', Apple_controlers.Apple_create_Page);
+router.get('/create', secured, Apple_controlers.Apple_create_Page);
 
 /* GET create update page */
-router.get('/update', Apple_controlers.Apple_update_Page);
+router.get('/update', secured, Apple_controlers.Apple_update_Page);
 
 /* GET delete Apple page */
-router.get('/delete', Apple_controlers.Apple_delete_Page);
+router.get('/delete', secured, Apple_controlers.Apple_delete_Page);
+
+module.exports = router;
