04d0eadd6c6bcf9385857f7f3ef082aca603957e
Kurri <S566991@nwmissouri.edu>
2023-11-29 16:53:23 -0600

    Updated app.js

--- app.js
+++ app.js
@@ -1,21 +1,89 @@
-require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    Account.findOne({ username: username })
+      .then(function (user){
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message: 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+      })
+      .catch(function(err){
+        return done(err)
+      })
+    })
+)
+
+require('dotenv').config();
+const connectionString = process.env.MONGO_CON
+mongoose = require('mongoose');
+mongoose.connect(connectionString,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+//Get the default connection
+var db = mongoose.connection;
+//Bind connection to error event
+db.on('error', console.error.bind(console, 'MongoDB connectionerror:'));
+db.once("open", function () {
+  console.log("Connection to DB succeeded")
+});
+
+var Apple = require('./models/Apple');
+
+async function recreateDB() {
+  // Delete everything
+  await Apple.deleteMany();
+  let instance1 = new Apple({
+    name: "red_Apple", color: "red",
+    price: 20
+  });
+
+  let instance2 = new Apple({
+  name: "green_Apple", color: "green",
+    price: 30
+  });
+
+  let instance3 = new Apple({
+    name: "yellow_Apple", color: "yellow",
+    price: 40
+  });
+
+  const newArray = [instance1.save(), instance2.save(), instance3.save()];
+  Promise.all(newArray).then(doc => {
+    console.log("First object saved")
+    console.log("Second object saved")
+    console.log("Third object saved")
+  }
+  ).catch(err => {
+    console.error(err)
+  });
+}
+let reseed = true;
+if (reseed) { recreateDB(); }
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var AppleRouter = require('./routes/Apple');
 var boardRouter = require('./routes/board');
 var chooseRouter = require('./routes/choose');
-var Apple = require('./models/Apple');
 var resourceRouter = require('./routes/resource');
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -23,6 +91,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+ }));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -32,17 +108,22 @@ app.use('/board', boardRouter);
 app.use('/choose', chooseRouter);
 app.use('/resource', resourceRouter);
 
-const connectionString = process.env.MONGO_CON
-mongoose = require('mongoose');
-mongoose.connect(connectionString);
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -51,45 +132,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-//Get the default connection
-var db = mongoose.connection;
-//Bind connection to error event
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once("open", function(){
-console.log("Connection to DB succeeded")});
- 
-async function recreateDB(){
-  // Delete everything
-  await Apple.deleteMany();
-  let instance1 = new
-  Apple({name:"red_Apple", color:"red",
-  price:20});
-  await instance1.save();
-  //instance1.save( function(err,doc) {
-  //if(err) return console.error(err);
-  console.log("First object saved")
-  //});
- 
-  let instance2 = new
-  Apple({name:"green_Apple", color:"green",
-  price:30});
-  await instance2.save();
-  //instance1.save( function(err,doc) {
-  //if(err) return console.error(err);
-  console.log("second object saved")
-  //});
- 
-  let instance3 = new
-  Apple({name:"yellow_Apple", color:"yellow",
-  price:40});
-  await instance3.save();
-  //instance1.save( function(err,doc) {
-  //if(err) return console.error(err);
-  console.log("Third object saved")
-  //});
- }
- let reseed = true;
- if (reseed) { recreateDB();}
 
 module.exports = app;
