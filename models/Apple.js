1768a089f681c7c57170a573641200ff3f737f32
Kurri <S566991@nwmissouri.edu>
2023-11-29 16:59:20 -0600

    Apple.js

--- models/Apple.js
+++ models/Apple.js
@@ -2,19 +2,38 @@ const mongoose = require("mongoose")
 const AppleSchema = mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        minlength: 3,
+        maxlength: 30,
+        // Custom validator using RegExp
+        validate: {
+            validator: function (v) {
+                return /^[a-zA-Z0-9_]+$/.test(v); // Ensure length of Apple name between 3 and 30
+            },
+            message: props => `${props.value} is not a valid username! Use only letters, numbers, and underscores.`,
+        },
     },
     color: {
         type: String,
-        required: true
+        required: true,
+        validate: {
+            validator: function(v) {
+                return /^[a-zA-Z0-9_]+$/.test(v); // Ensure price is non-negative
+            },
+            message: props => `${props.value} is not a valid color! Color must be a non-negative number.`,
+        },
     },
     price: {
         type: Number,
-        required: true
-    }
-});
-module.exports = mongoose.model("Apple",
-AppleSchema)
+        validate: {
+            validator: function(v) {
+                return v >= 0; // Ensure price is non-negative
+            },
+            message: props => `${props.value} is not a valid price! Price must be a non-negative number.`,
+        },
+    },
+})
+module.exports = mongoose.model("Apple", AppleSchema)
 
     
     
